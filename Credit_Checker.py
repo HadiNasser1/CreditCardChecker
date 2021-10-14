4c6615eeabc81040a9634ec3009fc80f056604c4
HadiNasser1 <92534692+HadiNasser1@users.noreply.github.com>
2021-10-14 18:49:31 +0200

    Update Credit_Checker.py

--- Credit_Checker.py
+++ Credit_Checker.py
@@ -1,4 +1,24 @@
-def luhnCheck(card_number)
-  
-  number_of_digits = len(card_number)
-  
+cardNo = list("4388576018402626")
+
+check_number = cardNo.pop()
+cardNo.reverse()
+
+changed_numbers = []
+
+for index, number in enumerate(cardNo):
+    if index % 2 == 0:
+        doubled_numbers = int(number) * 2
+
+        if doubled_numbers > 9:
+            doubled_numbers = doubled_numbers - 9
+
+        changed_numbers.append(doubled_numbers)
+    else:
+        changed_numbers.append(int(number))
+
+total_sum = int(check_number) + sum(changed_numbers)
+
+if total_sum % 10 == 0:
+    print("This credit card number is valid")
+else:
+    print("This credit card number is not valid")

97a8a91c3d3bcc4c03df2dd0a03112ee56b9c223
HadiNasser1 <92534692+HadiNasser1@users.noreply.github.com>
2021-10-22 16:33:54 +0200

    Update Credit_Checker.py

--- Credit_Checker.py
+++ Credit_Checker.py
@@ -11,42 +11,47 @@ length = len(cardNo)
 # We turn the numbers into a list so we can index it later on for Luhn's algorithm.
 cardNo_list = list(cardNo)
 
-# We use an if loop here to check the length of the number entered. If it is lower than 13 digits than it will display an error saying the number is too short.
-#If the number is larger than 16 than it will display an error message saying the number is too long
-if length < 13:
-    print ("This credit card number is not valid. It is too short.")
-elif length > 16:
-    print ("This credit card number is not valid. It is too long.")
+# We make an if loop here to check if the credit card number is legitemate before we apply Luhn's to check if it is valid.
+if cardNo[0] != '4' and cardNo[0] != '5' and cardNo[0] != '6' and cardNo[0:2] != "37":
+    print ('This credit card number is not valid.')
 else:
 
-# this takes the last number out of the list, making it our check number for later use.
-    check_number = cardNo_list.pop()
-# reverses the code as it makes it easier for us to use the algorithm
-    cardNo_list.reverse()
-
-# We create an empty list to store our new numbers in later on.
-    changed_numbers = [] 
-
-    # We use a for loop here to iterate the sequence and go through each element of list. Enumerate() is used here to make an index of every number in the last, allowing us to run Luhn's algorithm.
-    for index, number in enumerate(cardNo_list):
-        # Using the condition statement, we check if the index number is even or not. If it is even then we double it.
-        if index % 2 == 0:
-            doubled_numbers = int(number) * 2 
-
-            # If the number is larger than 9, then we subtract 9 and use the remainder instead.
-            if doubled_numbers > 9:
-                doubled_numbers = doubled_numbers - 9
-                        
-            # This changes the empty list by adding the even numbers into it after they were doubled and adding the odd numbers in without any changes.
-            changed_numbers.append(doubled_numbers)
-        else:
-            changed_numbers.append(int(number))
-
-    # this the second to last step following luhns algorithm and is adding our check number to the sum of the changed numbers
-    total_sum = int(check_number) + sum(changed_numbers)   
-                    
-    # if sum is divisable by 10 we know that the crad number is valid according to luhns algorithm
-    if total_sum % 10 == 0:
-        print("This credit card number is valid.") 
+    # We use an if loop here to check the length of the number entered. If it is lower than 13 digits than it will display an error saying the number is too short.
+    #If the number is larger than 16 than it will display an error message saying the number is too long
+    if length < 13:
+        print ("This credit card number is not valid. It is too short.")
+    elif length > 16:
+        print ("This credit card number is not valid. It is too long.")
     else:
-        print("This credit card number is not valid.")
+
+    # this takes the last number out of the list, making it our check number for later use.
+        check_number = cardNo_list.pop()
+    # reverses the code as it makes it easier for us to use the algorithm
+        cardNo_list.reverse()
+
+    # We create an empty list to store our new numbers in later on.
+        changed_numbers = [] 
+
+        # We use a for loop here to iterate the sequence and go through each element of list. Enumerate() is used here to make an index of every number in the last, allowing us to run Luhn's algorithm.
+        for index, number in enumerate(cardNo_list):
+            # Using the condition statement, we check if the index number is even or not. If it is even then we double it.
+            if index % 2 == 0:
+                doubled_numbers = int(number) * 2 
+
+                # If the number is larger than 9, then we subtract 9 and use the remainder instead.
+                if doubled_numbers > 9:
+                    doubled_numbers = doubled_numbers - 9
+                            
+                # This changes the empty list by adding the even numbers into it after they were doubled and adding the odd numbers in without any changes.
+                changed_numbers.append(doubled_numbers)
+            else:
+                changed_numbers.append(int(number))
+
+        # this the second to last step following luhns algorithm and is adding our check number to the sum of the changed numbers
+        total_sum = int(check_number) + sum(changed_numbers)   
+                        
+        # if sum is divisable by 10 we know that the crad number is valid according to luhns algorithm
+        if total_sum % 10 == 0:
+            print("This credit card number is valid.") 
+        else:
+            print("This credit card number is not valid.")
